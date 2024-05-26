c29f2d49b4220255e84ec19962137215004d1c56
ananya <aananya30121998@gmail.com>
2024-05-26 12:31:58 +0530

    newly added changes

--- code/app.py
+++ code/app.py
@@ -127,22 +127,22 @@ def logout():
     return redirect('/')
 
 # load model
-# detect
-@app.route('/detect/<int:id>')
-def detect_forgery(id):
-    db =  open_db()
-    img = db.query(File).get(id)
-    path = img.path
-    if os.path.exists(path):
-        image = prepare_image(path)
-        image = image.reshape(-1, 128, 128, 3)
-        model = load_my_model('models\model_casia_run1.h5')
-        print('model loaded')
-        y_pred = model.predict(image)
-        y_pred_class = np.argmax(y_pred, axis=1)[0]
-        cls = class_names[y_pred_class]
-        conf  = np.amax(y_pred)*100
-        if y_pred_class == 0:
+# detection
+@app.route('/detect/<int:id>')   # This part defines a route /detect/<int:id> using Flask's @app.route decorator. It expects an integer ID as a parameter in the URL.
+def detect_forgery(id):  # This function detect_forgery is triggered when the /detect/<int:id> route is accessed. It takes an integer ID as a parameter.
+    db =  open_db()     # This line opens a connection to the database.
+    img = db.query(File).get(id)    # It retrieves a record from the File table in the database based on the provided ID.
+    path = img.path  # It extracts the file path from the retrieved record.
+    if os.path.exists(path):  # This condition checks if the file exists at the specified path.
+        image = prepare_image(path)  # It prepares the image located at the specified path for processing.
+        image = image.reshape(-1, 128, 128, 3)   # Reshapes the image data into the required format for the model. Here, the image is being reshaped to match the input dimensions expected by the model.
+        model = load_my_model('models\model_casia_run1.h5')   # Loads a pre-trained model from the specified path.
+        print('model loaded') 
+        y_pred = model.predict(image)  # This line is asking the model to analyze the image and provide a set of probabilities indicating how likely it is that the image belongs to each of the classes the model can recognize. The result (y_pred) is a list of these probabilities.
+        y_pred_class = np.argmax(y_pred, axis=1)[0]  # The line finds the index of the highest probability in the array of predicted probabilities (y_pred). This index represents the predicted class for the input image.
+        cls = class_names[y_pred_class]   # Maps the predicted class index to a human-readable class name using class_names.
+        conf  = np.amax(y_pred)*100   # Calculates the confidence score by finding the maximum probability and scaling it to a percentage.
+        if y_pred_class == 0:  # Checks if the predicted class is 0.
             try:
                 os.makedirs('static/ela', exist_ok=True)
                 ela_path = f'static/ela/{img.id}.png'
@@ -153,12 +153,17 @@ def detect_forgery(id):
                 # modified_path = f'static/modified/{img.id}.png'
                 # modify_boundary.save(modified_path)
                 return render_template('result.html', img = img, cls = cls, conf = conf, ela_path = ela_path)
+             # If the predicted class is 0, it performs error level analysis (ELA) on the image, saves the result, and renders a template `result.html` passing the image details, predicted class, confidence score, and ELA path.
             except Exception as e:
                 print(e)
-        return render_template('result.html', img = img, cls = cls, conf = conf)
+        return render_template('result.html', img = img, cls = cls, conf = conf)  # Renders a template `result.html` with image details, predicted class, and confidence score if the predicted class is not 0.
     else:
-        flash('file not found','danger')
+        flash('File not found','danger')  # If the file does not exist, it flashes a message indicating that the file was not found.
     return redirect('/file/list')
+
+# Recompression: The image is saved again at a specified quality level.
+# Difference Calculation: The difference between the original image and the recompressed image is calculated to highlight discrepancies.
+# 91 is the quality level used during the ELA process to recompress the image and help highlight areas that might have been manipulated.
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
